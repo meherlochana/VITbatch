e3c5c1c44699a5acb9bbea6fa1f09b8a380621d0
meher <meherlochanachinathamakula@gmail.com>
2024-06-22 19:35:11 +0530

    button click-log

--- app/src/main/java/com/example/vitbatch/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.vitbatch
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun myclickhandeler(view: View) {
+        Log.i("Mainactivity", "button clicked")
     }
 }
