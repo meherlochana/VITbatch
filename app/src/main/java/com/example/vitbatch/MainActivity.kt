950049068daea2faec42dd2ec2f90a2239e98501
meher <meherlochanachinathamakula@gmail.com>
2024-06-26 15:37:18 +0530

    alaram

--- app/src/main/java/com/example/vitbatch/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch/MainActivity.kt
@@ -5,6 +5,7 @@ package com.example.vitbatch
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -28,10 +29,22 @@ class MainActivity : AppCompatActivity() {
     fun myclickhandeler(view: View) {
         Log.i("Mainactivity", "button clicked")
         //var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:923452713") )
-        var webIntent:Intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://yahoo.com"))
-        startActivity(webIntent)
+        /*var webIntent:Intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://yahoo.com"))
+        startActivity(webIntent)*/
+        createAlarm("vit",15,21)
 
 
+    }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        //if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        //}
     }
 
 
